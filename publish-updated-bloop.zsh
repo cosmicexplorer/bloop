a344c704d435c8578d93686d6d950d1d78221d15
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-07-05 23:19:19 -0700

    add magic bloop update script

---
+++ publish-updated-bloop.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+set -euxo pipefail
+
+sbt publishLocal publishM2 \
+  &> >(tee /dev/stderr) \
+  | sed -rne 's#^.*bloop-frontend_2.12-(1.3.2.*)\.pom.*#\1#gp' \
+  | head -n1 \
+         > ./BLOOP_VERSION
+
+BLOOP_VERSION="$(cat ./BLOOP_VERSION)"
+
+sed -r -i -e "s#(BLOOP_REV_TO_FIND=)(.*$)#\\1'${BLOOP_VERSION}'#g" \
+    ./bloop-invoke/blp-server
+
+pushd "$OS_PANTS_SRC"
+sed -r -i -e "s#(BLOOP_REV = )(.*$)#\1'${BLOOP_VERSION}'#g" \
+    contrib/bloop/3rdparty/jvm/BUILD
+popd
